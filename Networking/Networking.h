54896aa0ac68d15d21a70fe728c6ebf19927a28c
Marius Landwehr <marius@deepl.com>
2020-05-11 14:37:41 +0200

    Initial Commit

---
+++ Networking/Networking.h
@@ -0,0 +1,13 @@
+//
+//  Networking.h
+//  Networking
+//
+//  Created by Marius Landwehr on 11.05.20.
+//  Copyright © 2020 DeepL. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Networking : NSObject
+
+@end
---
+++ Networking/Networking.m
@@ -0,0 +1,13 @@
+//
+//  Networking.m
+//  Networking
+//
+//  Created by Marius Landwehr on 11.05.20.
+//  Copyright © 2020 DeepL. All rights reserved.
+//
+
+#import "Networking.h"
+
+@implementation Networking
+
+@end
